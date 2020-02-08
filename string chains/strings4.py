9e07f292255744d7c19e20ef7217a318c431efcf
Saul <saulc@users.noreply.github.com>
2020-02-08 13:56:37 -0800

    so close..
    ...but so far..

--- string chains/strings4.py
+++ string chains/strings4.py
@@ -34,6 +34,7 @@ def makeChains(words):
     chain = []
     wordcheck = []
     for i in range(0, len(words) ):
+        # print(i )
         se = [False, False] #used as start word, end word
         wordcheck.append(se)
 
@@ -45,20 +46,40 @@ def makeChains(words):
             #check the rest of the words for matching links
             bword = words[j]
             print("   " + str(j) + " ~ " + bword )
-            if checkLinks(aword, bword) :
-                chain.append(aword)
-                chain.append(bword) 
+            if wordcheck[j][0] == False and wordcheck[j][1] == False:
+                temp = checkLinks(aword, bword)
+                print("Check state: " + str(temp) )
+                if temp == 1:   #word have not been swapped
+                    wordcheck[i][0] = True
+                    wordcheck[j][1] = True
+
+                    chain.append(aword)
+                    chain.append(bword)
+                elif temp == 2: #words have been swapped, swap flag indexes to match.
+                    wordcheck[j][0] = True
+                    wordcheck[i][1] = True
+                    chain.append(bword)
+                    chain.append(aword)
 
     print(chain)
+    k = 0
+    for i in  wordcheck:
+        print("word check: " + str(i) + " = "+  words[k] )
 
+        k+= 1
+
+# compare words, return 0 for no match,
+# 1 if end of a == start of b
+# 2 if end of b == start of a
 def checkLinks(a, b):
     print("      " + a + " " + b)
     s , e = getEnds(a)
     ss , ee = getEnds(b)
     if e == ss :
-        return a, b
+        return 1
     elif s == ee:
-        return b, a
+        return 2
+    return 0
     # st = "start: " + s + " end:" + e
     # print("end" + st)
 
